562825a23f4e02b5e2c29031651b44512a596031
blakemwp <blakemwp@gmail.com>
2025-11-26 11:29:06 -0500

    Idk what i'm doing lowkey

--- Scenes/battle.gd
+++ Scenes/battle.gd
@@ -1,4 +1,14 @@
 extends Control
 
-@onready var _options: NinePatchRect = $Options
+@onready var _options: WindowDefault = $Options
 @onready var _options_menu: VBoxContainer = $Options/Menu
+
+func _ready() -> void:
+	_options_menu.button_focus(0)
+
+func _on_menu_button_focused(button: BaseButton) -> void:
+	pass # Replace with function body.
+
+
+func _on_menu_button_pressed(button: BaseButton) -> void:
+	print(button.text)
